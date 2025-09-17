29c4d4d609c43892d175ad057b34638262fc61fa
Youcef NAMOUN <youcef.na@kuikops.com>
2025-09-17 16:18:33 +0200

    init projet

---
+++ app/main.py
@@ -0,0 +1,17 @@
+
+def dire_bonjour(nom):
+    print(f"Bonjour, {nom} !")
+
+def addition(a, b):
+    resultat = a + b
+    print(f"La somme de {a} et {b} est {resultat}")
+
+def afficher_liste(liste):
+    print("Voici la liste :")
+    for element in liste:
+        print(f"- {element}")
+
+if __name__ == "__main__":
+    dire_bonjour("Youcef")
+    addition(3, 7)
+    afficher_liste(["pomme", "banane", "orange"])
